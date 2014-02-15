07dbe6f2fd7fe98e564dc3f1c71c40404d3b9207
John Cant <a.johncant@gmail.com>
2014-02-15 16:06:49 +0000

    Fix path

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 import BaseHTTPServer
 from json_conversion import *
+import re
+import stat
+import os
 
-path_to_vomiting = '../data/ASB Noise Cleansing Waste Washing/vomiting-lat-long.csv'
+path_to_vomiting = './vomiting-lat-long.csv'
 
 #data = ConvertToJSON(path_to_vomiting, 'F')
 
 class ApiHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(s):
+
+    def api_endpoint(s, path):
         s.send_response(200)
         s.send_header("Content-type", "application/json")
         s.end_headers()
 
-
         s.wfile.write(ConvertToJSON(path_to_vomiting, 'F'))
 
+    def static_file(s, path):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+
+        if stat.S_ISDIR(os.stat(s.path).st_mode):
+            path = os.path.join(os.getcwd(), 'public', 'index.html')
+        else:
+            path = os.getcwd() + '/public' + path
+
+        f = open(path, 'r')
+        s.wfile.write(f.read());
+
+    def do_GET(s):
+
+        api_matcher = re.compile('/api(/|$)', re.IGNORECASE)
+        if api_matcher.match(s.path):
+            s.api_endpoint(s.path)
+        else:
+            s.static_file(s.path)
 
 server_class = BaseHTTPServer.HTTPServer
 httpd = server_class(("localhost", 6080), ApiHandler)
